66e8ca22ac077805aa21fbe59139c7aa9ba7ca4f
huydaungoc <huydaungoc>
2018-07-05 12:14:12 +0700

    add auto call sample (make outbound call from your server)

---
+++ auto-call/auto-call.php
@@ -0,0 +1,43 @@
+<?php
+
+include './StringeeApi/StringeeCurlClient.php';
+
+global $config;
+
+
+$keySid = 'YOUR_KEY_SID';
+$keySecret = 'YOUR_KEY_SECRET';
+
+$curlClient = new StringeeCurlClient($keySid, $keySecret);
+$url = 'http://api.stringee.com/v1/call2/callout';
+
+$data = '{
+    "from": {
+        "type": "external",
+        "number": "YOUR_STRINEE_NUMBER",
+        "alias": "YOUR_STRINEE_NUMBER"
+    },
+    
+    "to": [{
+        "type": "external",
+        "number": "CALL_TO_NUMBER",
+        "alias": "CALL_TO_NUMBER"
+    }],
+   
+    "actions": [{
+        "action": "talk",
+        "text": "       Stringee kính chào quý khách, đây là cuộc gọi tự động, vui lòng liên hệ với chúng tôi qua info@stringee.com",
+        "voice": "hn_male_xuantin_vdts_48k-hsmm"
+    }]
+}';
+
+$resJson = $curlClient->post($url, $data, 15);
+$status = json_decode($resJson->getStatusCode());
+
+$content = $resJson->getContent();
+
+echo '$status: ' . $status . '<br>';
+echo '$content: ' . $content . '<br>';
+
+
+
