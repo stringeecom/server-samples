0ea930a9d5a70f62dc5ebacc22021fe951dd68d9
huydaungoc <huydaungoc>
2018-07-08 13:32:50 +0700

    add sample gen access_token for REST API call

---
+++ access_token/php/access_token_for_rest_api.php
@@ -0,0 +1,50 @@
+<?php
+
+include 'FirebaseJWT/JWT.php';
+
+use \Firebase\JWT\JWT;
+
+/*
+HEADER:
+	{
+	  "typ": "JWT",
+	  "alg": "HS256",
+	  "cty": "stringee-api;v=1"
+	}
+
+PAYLOAD:
+	{
+	  "jti": "SK...-1497503680",//JWT ID
+	  "iss": "SK...",//API Key SID
+	  "exp": 1497507280,//Expiration Time
+	  "userId": "huydn-123456"
+	}
+*/
+
+$apiKeySid = 'YOUR_API_KEY_SID';
+$apiKeySecret = "YOUR_API_KEY_SECRET";
+
+$now = time();
+$exp = $now + 3600;
+
+$header = array('cty' => "stringee-api;v=1");
+$payload = array(
+    "jti" => $apiKeySid . "-" . $now,
+    "iss" => $apiKeySid,
+    "exp" => $exp,
+    "rest_api" => true
+);
+
+$jwt = JWT::encode($payload, $apiKeySecret, 'HS256', null, $header);
+
+$res = array(
+	'access_token' => $jwt
+	);
+
+header('Access-Control-Allow-Origin: *');
+echo json_encode($res);
+
+
+
+
+
